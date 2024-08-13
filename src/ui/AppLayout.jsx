b7b5a7d74b4b8edd245d4d775ba4773b20b46617
Alexander Ivanov <saudio.ru@gmail.com>
2024-08-13 16:56:31 +0200

    adding navigation

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,31 @@
+import { Outlet } from "react-router-dom";
+import styled from "styled-components";
+
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+const StyledApp = styled.div`
+  height: 100vh;
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+function AppLayout() {
+  return (
+    <StyledApp>
+      <Header />
+      <Sidebar />
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledApp>
+  );
+}
+
+export default AppLayout;
---
+++ src/ui/Sidebar.jsx
@@ -0,0 +1,27 @@
+import styled from "styled-components";
+
+import Logo from "./Logo";
+import MainNav from "./MainNav";
+
+const StyleSidebar = styled.aside`
+  background-color: var(--color-grey-0);
+  padding: 3.2rem 2.4rem;
+  border-right: 0.1rem solid var(--color-grey-100);
+
+  grid-row: 1/ -1;
+
+  display: flex;
+  flex-direction: column;
+  gap: 3.2rem;
+`;
+
+function Sidebar() {
+  return (
+    <StyleSidebar>
+      <Logo />
+      <MainNav />
+    </StyleSidebar>
+  );
+}
+
+export default Sidebar;
